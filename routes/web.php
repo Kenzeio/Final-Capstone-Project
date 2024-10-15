cd1825b1fb9eb2945112d490cf30455b073856ae
Kenzeio <115766913+Kenzeio@users.noreply.github.com>
2024-10-15 23:53:35 +0800

    kani pa bro

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,14 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SportController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\VenueController;
+use App\Http\Controllers\PointsController;
 
 Route::resource('product', ProductController::class);
 Route::resource('college', CollegeController::class);
 Route::resource('venue', VenueController::class);
 Route::resource('sport', SportController::class);
-Route::resource('palakasan', PalakasanController::class);
-Route::put('SSCAdmin/palakasan/{id}', [PalakasanController::class, 'update'])->name('palakasan.update');
 
-// Use resource for common CRUD routes
-Route::resource('student', StudentController::class);
-Route::post('student/import', [StudentController::class, 'import'])->name('student.import');
+
 
 Route::get('/', function () {
     return Inertia::render('SSCAdmin/Dashboard');
